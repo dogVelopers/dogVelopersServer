96f14f6b24e69b25ae54b7b9226ddcb4f2162f18
jaeyeon kim <jakind@naver.com>
2022-02-12 11:47:06 +0900

    [#3]requestDto , responseDto 생성 및 , member , hof api 수정

--- dogvelopers/src/main/java/com/dogvelopers/dogvelopers/service/MemberService.java
+++ dogvelopers/src/main/java/com/dogvelopers/dogvelopers/service/MemberService.java
@@ -6,6 +6,8 @@ import com.dogvelopers.dogvelopers.entity.Member;
 import com.dogvelopers.dogvelopers.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 import java.util.*;
 
 @Service
@@ -15,6 +17,17 @@ public class MemberService {
     private final MemberRepository memberRepository;
 
     public MemberResponseDto save(MemberRequestDto memberRequestDto){
+
+        // 생일이 잘못 되어 있으면
+        if(memberRequestDto.getBirthDay() == null || memberRequestDto.getBirthDay().getYear() > LocalDateTime.now().getYear()){
+            return null; // exception 처리
+        }
+
+        // 이름 , 학번 , 전공 입력이 안되어있으면
+        if(memberRequestDto.getStudentId() == null || memberRequestDto.getStudentId().isBlank() || memberRequestDto.getName() == null || memberRequestDto.getName().isBlank() || memberRequestDto.getMajor() == null || memberRequestDto.getMajor().isBlank()){
+            return null; // exception 처리
+        }
+
         return new MemberResponseDto(memberRepository.save(memberRequestDto.toEntity()));
     }
 
