65db9a1f33fa797d72cebcb9bee00f3a007585ce
jaeyeon kim <jakind@naver.com>
2022-02-13 22:14:19 +0900

    api url 복수형으로 변경

--- dogvelopers/src/main/java/com/dogvelopers/dogvelopers/controller/HofController.java
+++ dogvelopers/src/main/java/com/dogvelopers/dogvelopers/controller/HofController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("hof")
+@RequestMapping("hofs")
 public class HofController {
 
     private final HofService hofService;
