ee037dde253dc788db6d3f49c88f0d824a240e03
eastheat10 <eastheat10@gmail.com>
2022-02-15 11:54:37 +0900

    [#3] 조회 기능 제작, application.yml 추가

--- dogvelopers/src/main/java/com/dogvelopers/dogvelopers/controller/ProjectController.java
+++ dogvelopers/src/main/java/com/dogvelopers/dogvelopers/controller/ProjectController.java
@@ -1,12 +1,30 @@
 package com.dogvelopers.dogvelopers.controller;
 
+import com.dogvelopers.dogvelopers.dto.ProjectRequestDto;
 import com.dogvelopers.dogvelopers.service.ProjectService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequestMapping("projects")
 @RequiredArgsConstructor
 public class ProjectController {
 
     private final ProjectService projectService;
+
+    @GetMapping
+    public ResponseEntity<ProjectRequestDto> findAll() {
+        return new ResponseEntity(projectService.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("{postId}")
+    public ResponseEntity<ProjectRequestDto> findById(@PathVariable("postId") Long id) {
+        return new ResponseEntity(projectService.findById(id), HttpStatus.OK);
+    }
+
 }
